da33d42ad5f667048c365438337b66d77bc581e8
Christopher Harrison <cnharrison@gmail.com>
2019-05-29 16:57:50 -0400

    helper functions

---
+++ data/db.js
@@ -0,0 +1,37 @@
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
+const db = knex(knexConfig);
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove
+};
+
+function find() {
+  return db("cohorts");
+}
+
+function findById(id) {
+  return db("cohorts").where({ id: Number(id) });
+}
+
+function insert(cohort) {
+  return db("cohorts")
+    .insert(cohort)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function update(id, cohort) {
+  return db("cohorts")
+    .where("id", Number(id))
+    .update(cohort);
+}
+
+function remove(id) {
+  return db("cohorts")
+    .where("id", Number(id))
+    .del();
+}
